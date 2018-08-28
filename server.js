0a81326ca5f0dcfd8e4d0b2fb3a39119c1d99ea8
sabyasacchiM <34243961+sabyasacchiM@users.noreply.github.com>
2018-08-28 15:41:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 var jsonData = require("./temp.js");
 var url = "temptest.herokuapp.com/";
 var currentdate = new Date();
-var datetime = " On DTS " + currentdate.getDay() + " " + currentdate.getMonth() + " " + currentdate.getFullYear() + " At IST " + currentdate.getHours() + "Hrs " + currentdate.getMinutes() + "Min " + currentdate.getSeconds() + "Sec";
+var datetime = " On ( " + currentdate.getDay() + " " + currentdate.getMonth() + " " + currentdate.getFullYear() + " ) At IST " + currentdate.getHours() + "Hrs " + currentdate.getMinutes() + "Min " + currentdate.getSeconds() + "Sec";
 console.log(datetime);
 
 app.get('/list', function (req, res) {
